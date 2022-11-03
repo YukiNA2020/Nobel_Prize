574117289cd9ad01b037544a7b91eba92575bb29
YukiNA2020 <1730731981@qq.com>
2022-11-03 16:07:21 +0800

    change some detail

--- final.py
+++ final.py
@@ -5,16 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 plt.style.use('seaborn')
-st.title("The nobel price ")
+st.title("The Nobel Prize ")
 df_date = pd.read_csv('nobel_prizes_by_date.csv')
 df_winner = pd.read_csv('nobel_prize_by_winner.csv')
 
 
-#处理csv
-#df_date.drop(['index', 'overallMotivation',"share"], axis=1, inplace=True)
-#df_date = df_date[df_date.firstname.notnull()].copy()
-#organisations = df_date[df_date.gender == 'org'].copy()
-#df_date = df_date[df_date.gender != 'org'].copy()
 
 #排序赋值
 df_winner=df_winner.sort_values(by='id',ascending=True)
@@ -34,7 +29,7 @@ df_winner = df_winner[df_winner.category.isin(subject_filter)]
 df_result = df_result[df_result.category.isin(subject_filter)]
 
 #上方拉条
-pop_filter = st.slider('the year of getting the prize',1901,2016,1945)
+pop_filter = st.sidebar.slider('the year of getting the prize',1901,2016,1945)
 df_winner = df_winner[df_winner['year'] <= pop_filter]
 df_result = df_result[df_result['year'] <= pop_filter]
 
@@ -46,6 +41,10 @@ ax.set_xlabel('Countries',fontsize=10)
 ax.set_ylabel('number of person/team',fontsize=10)
 country.plot.bar()
 st.pyplot(fig)
+
+#问题分析
+st.write('What is the relationship between people won the Nobel Prize and the level of development of country?')
+st.write('As we can see from the graph, countries with higher levels of economic development have won more Nobel Prizes. For example, the United States, UK, which is inseparable from their strong comprehensive national power')
 
 #显示学科
 st.subheader('the subject of the winner')
@@ -67,3 +66,8 @@ ax[0].set_xlabel('age')
 ax[0].set_ylabel('the numeber')
 df_result.awarded_at.plot.box(ax=ax[1])
 st.pyplot(fig)
+
+
+#第二个问题分析
+st.write('What is the relationship between people won the Nobel Prize and their age?')
+st.write('We can see from these two figures, most of the people are between 45 and 65 years old to win the Nobel Prize, indicating that this is the best time for people to contribute to society')
